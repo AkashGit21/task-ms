6ef89259590e1544606a6bb8a52c1e9517db6483
Akash Gupta <akashgzb98@gmail.com>
2025-03-23 17:03:31 +0530

    Add interface for tasks API

--- internal/task/handler.go
+++ internal/task/handler.go
@@ -1,6 +1,8 @@
 package task
 
 import (
+	"net/http"
+
 	"github.com/AkashGit21/task-ms/lib/persistence/mysql"
 	"github.com/gorilla/mux"
 )
@@ -24,4 +26,13 @@ func TaskV1Handler(r *mux.Router) {
 	th := newTaskAPIHandler()
 
 	r.HandleFunc("/tasks", th.createTask).Methods("POST")
+	r.HandleFunc("/tasks/{taskID}", th.getTask).Methods("GET")
+	r.HandleFunc("/tasks/{taskID}", th.patchTask).Methods("PATCH")
+	r.HandleFunc("/tasks/{taskID}", th.deleteTask).Methods("DELETE")
+	r.HandleFunc("/tasks/{taskID}", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	}).Methods("OPTIONS")
+	r.HandleFunc("/tasks", th.listTasks).Methods("GET")
 }
--- internal/task/service.go
+++ internal/task/service.go
@@ -7,3 +7,19 @@ import (
 /** TODO: Creates a new Task **/
 func (tvh *TaskHandler) createTask(w http.ResponseWriter, r *http.Request) {
 }
+
+/** TODO: Get the Task with ID **/
+func (tvh *TaskHandler) getTask(w http.ResponseWriter, r *http.Request) {
+}
+
+/** TODO: Patch certain fields of Task with given ID **/
+func (tvh *TaskHandler) patchTask(w http.ResponseWriter, r *http.Request) {
+}
+
+/** TODO: Perform soft delete of Task with given ID **/
+func (tvh *TaskHandler) deleteTask(w http.ResponseWriter, r *http.Request) {
+}
+
+/** TODO: List the available Tasks with filters in paginated view **/
+func (tvh *TaskHandler) listTasks(w http.ResponseWriter, r *http.Request) {
+}
